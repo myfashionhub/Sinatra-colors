684da15edc3f0901df0ad023f2b1c9d11966cc75
Nessa Nguyen <v.nessa.nguyen@gmail.com>
2014-07-03 15:03:27 -0400

    Add db url

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require 'bundler'
 Bundler.require
 
 db_url = ENV['DATABASE_URL'] || 'postgres://postgres:postgres@localhost/sinatra_colors_development'
-db = URI.parse()
+db = URI.parse(db_url)
 
 ActiveRecord::Base.establish_connection(
   :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
