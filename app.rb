02b0cfd71ec6ca8373ac0a40ef1b25509aebf16c
Nessa Nguyen <v.nessa.nguyen@gmail.com>
2014-07-03 14:17:05 -0400

    Config database url for heroku

--- app.rb
+++ app.rb
@@ -1,15 +1,8 @@
 require 'bundler'
 Bundler.require
 
-db = URI.parse('postgres://postgres:postgres@localhost/sinatra_colors_development')
-
 ActiveRecord::Base.establish_connection(
-  :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-  :host     => db.host,
-  :username => db.user,
-  :password => db.password,
-  :database => db.path[1..-1],
-  :encoding => 'utf8'
+  ENV['DATABASE_URL'] || 'postgres://postgres:postgres@localhost/sinatra_colors_development'
 )
 
 class Palette < ActiveRecord::Base
